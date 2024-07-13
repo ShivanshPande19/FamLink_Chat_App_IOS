3dba70fcf4a3d44fbd5d0b8dc41bc6b28ee81261
ShivanshPande19 <pshivanshofficial@gmail.com>
2024-07-13 16:41:32 +0530

    Initial Commit

---
+++ FamLink_Chat_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamLink_Chat_App
+//
+//  Created by SHIVANSH PANDE on 13/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FamLink_Chat_App/FamLink_Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamLink_Chat_AppApp.swift
+//  FamLink_Chat_App
+//
+//  Created by SHIVANSH PANDE on 13/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FamLink_Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
